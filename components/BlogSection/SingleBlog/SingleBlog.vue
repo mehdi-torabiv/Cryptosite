da79069eef36f755e429add441747bf4dc8f5553
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-05-11 12:52:48 +0430

    bind img to child components with props to be dynamic

--- components/BlogSection/SingleBlog/SingleBlog.vue
+++ components/BlogSection/SingleBlog/SingleBlog.vue
@@ -1,12 +1,33 @@
 <template>
-  <div id="singleBlog">
-    <div class="blog-img">
+  <div id="singleBlog" class="md:w-1/3 justify-center">
+    <div class="blog-img" slot="blogimg">
       <a href="#">
-        <img src alt class="video-ifram" />
+        <img :src="imagedata" alt class="video-ifram" />
       </a>
       <a href>
         <play-icon class="custom-class"></play-icon>
       </a>
+    </div>
+    <div class="blog-title">
+      <span class="text-sm">12 مهرماه 1399</span>
+      <h3 class="mt-3 font-normal">
+        <a href>IBM اثبات برای کار بلاکچین</a>
+      </h3>
+      <div class="blog-meta mt-3 ">
+        <span class="ago text-sm text-gray-600">1 ساعت پیش</span>
+        <ul class="share_options float-left ">
+          <li>
+            <a href>
+              <img src="../../../assets/images/blog/bicon-01.png" alt />
+            </a>
+          </li>
+          <li>
+            <a href>
+              <img src="../../../assets/images/blog/bicon-02.png" alt />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -15,11 +36,39 @@
 import { PlayIcon } from "vue-bytesize-icons";
 
 export default {
-  components: {
-    PlayIcon
-  }
+  props: {
+    imagedata: {
+      type: String,
+      default: null
+    }
+  },
+  components: { PlayIcon }
 };
 </script>
 
 <style>
+#singleBlog {
+  padding: 15px;
+  background: blanchedalmond;
+  margin-bottom: 30px;
+  max-width: 33.3333%;
+  margin-left: 1rem;
+}
+#singleBlog .blog-img img {
+  border-radius: 3px;
+}
+#singleBlog > a > img {
+  max-width: 100%;
+  height: auto;
+}
+#singleBlog > .blog-img:nth-child(2) #i-play {
+  position: relative;
+  left: 0 !important;
+}
+#singleBlog .share_options li {
+  display: inline-block;
+}
+#singleBlog .share_options > li > a > img {
+  vertical-align: middle;
+}
 </style>
--- pages/blog.vue
+++ pages/blog.vue
@@ -6,19 +6,34 @@
     <div class="container mx-auto">
       <div class="sub-title-blog py-12">
         <h2 class="header-title-blog text-center">بلاگ و رسانه سایت نیکوبیت</h2>
-        <p
-          class="subtitle-blog text-center text-gray-600"
-        >پیشرفت چشمگیر در بیزینس به واسطه سایت نیکوبیت</p>
+        <p class="subtitle-blog text-center text-gray-600">
+          پیشرفت چشمگیر در بیزینس به واسطه سایت نیکوبیت
+        </p>
+      </div>
+      <div class="blogs inline-flex">
+        <SingleBlog :imagedata="Blogimg1"></SingleBlog>
       </div>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import NavigationBar from "../components/NavigationBar/NavigationBar";
+import SingleBlog from "../components/BlogSection/SingleBlog/SingleBlog";
+import Footer from "../components/Footer/Footer";
+
+import Blog3 from "../assets/images/blog/blog-03.jpg";
 export default {
+  data() {
+    return {
+      Blogimg1: Blog3
+    };
+  },
   components: {
-    NavigationBar: NavigationBar
+    NavigationBar: NavigationBar,
+    SingleBlog,
+    Footer
   }
 };
 </script>
